aadc89022a2ad8d5bbe3487b1e16cb30c67b8660
Sun Jiaqi <s1479843569@163.com>
2024-08-14 22:25:51 +0800

    fix

--- pk-community/src/main/java/io/yyy/constant/Commons.java
+++
@@ -1,6 +0,0 @@
-package io.yyy.constant;
-
-public interface Commons {
-    int ROOM_NUM_CLEAR = 10;
-    int OU_NUM = 2;
-}
--- pk-community/src/main/java/io/yyy/constant/Room.java
+++ pk-community/src/main/java/io/yyy/constant/Room.java
@@ -1,8 +1,5 @@
 package io.yyy.constant;
 
 public interface RobotName {
-    String A = "程序员";
-    String B = "程序媛";
-
     int ROOM_NUM_CLEAR = 10;
 }
